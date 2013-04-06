54494392bf80e0b034712de19ee9e077dc6a8402
FrozenFeather <brianvong1003@gmail.com>
2013-04-06 20:52:53 +0800

    Update hegmonyextensions-ai.lua

--- lua/ai/hegmonyextensions-ai.lua
+++ lua/ai/hegmonyextensions-ai.lua
@@ -1,1 +1,12 @@
 sgs.ai_skill_invoke["HegLuoshen"] = sgs.ai_skill_invoke.luoshen
+
+sgs.ai_skill_invoke["HegJizhi"] = function(self, data)
+	return true
+end
+function sgs.ai_cardneed.jizhi(to, card)
+	return card:isNDTrick()
+end
+
+sgs.ai_skill_invoke["HegXiaoji"] = function(self, data)
+	return true
+end
